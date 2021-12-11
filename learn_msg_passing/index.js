8455049fa873b9504beeefc00855f9bb55e0208a
Kr-Shanu <80747792+Kr-Shanu@users.noreply.github.com>
2021-12-11 22:25:13 +0530

    latest update in index.js

--- learn_msg_passing/index.js
+++ learn_msg_passing/index.js
@@ -1,11 +1,12 @@
-document.querySelector("button").addEventListener("click", sendMessage);
+document.getElementById("pass").addEventListener("click", sendMessage);
+
 
 
 // Sending message to the background script.
 function sendMessage()
 {
-    const minutes = parseInt(document.querySelector("input").value);
-    chrome.runtime.sendMessage({minutes}, function(response) {
-        console.log(response.success);
+    const minutes = parseInt(document.querySelectorAll("input")[0].value);
+    chrome.runtime.sendMessage({minutes, type : "m1"}, function(response) {
+        // console.log(response.success);
       });
 }
